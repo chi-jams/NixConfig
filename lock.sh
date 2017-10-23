d07405546a477986da247aa5b4a297255b192cae
daichij <djameson@mines.edu>
2017-10-23 16:39:47 -0600

    changed lock to use jpegs instead of pngs for reading since they read a lot faster

--- lock.sh
+++ lock.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 icon="$HOME/.xlock/icon.png"
-tmpbg='/tmp/screen.png'
+tmpbg='/tmp/screen.jpeg'
+outbg='/tmp/screen.png'
 scrot "$tmpbg"
-convert "$tmpbg" -scale 4% -scale 2500% "$tmpbg"
+convert "$tmpbg" -scale 4% -scale 2500% "$outbg"
 # convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 #i3lock -u -i "$tmpbg"
-i3lock -i "$tmpbg"
+i3lock -i "$outbg"
+
 rm "$tmpbg"
+rm "$outbg"
