44d5c35810394834a9ddb53e16113eb4d9c362e3
daichij <djameson@mines.edu>
2017-10-25 02:40:00 -0600

    Changed from using convert to img_scale on lock screen

--- lock.sh
+++ lock.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-icon="$HOME/.xlock/icon.png"
 tmpbg='/tmp/screen.jpeg'
 outbg='/tmp/screen.png'
 scrot "$tmpbg"
-convert "$tmpbg" -scale 4% -scale 2500% "$outbg"
+
+#convert "$tmpbg" -scale 4% -scale 2500% "$outbg"
+$HOME/NixConfig/img_scale "$tmpbg" "$outbg"
 # convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 #i3lock -u -i "$tmpbg"
 i3lock -i "$outbg"
