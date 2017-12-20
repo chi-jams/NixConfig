a2f06e135589a9184bddd6ef2b22e90657cc8909
daichij <djameson@mines.edu>
2017-12-20 16:57:48 -0700

    Updated setup_i3 to be a bit smarter

--- setup_i3.sh
+++ setup_i3.sh
@@ -1,8 +1,19 @@
 
 i3cfg_location=$(readlink -f i3config)
-mkdir $HOME/.config/i3
+if [ ! -d $HOME/.config/i3 ]; then
+    mkdir $HOME/.config/i3
+fi
+if [ $HOME/.config/i3/config ]; then
+    mv $HOME/.config/i3/config $HOME/.config/i3/config.bak
+fi
+
 ln -s $i3cfg_location $HOME/.config/i3/config
 
 i3status_cfg=$(readlink -f i3status.conf)
-mkdir $HOME/.config/i3status
+if [ ! -d $HOME/.config/i3status ]; then
+    mkdir $HOME/.config/i3status
+fi
+if [ $HOME/.config/i3status/config ]; then
+    mv $HOME/.config/i3status/config $HOME/.config/i3status/config.bak
+fi
 ln -s $i3status_cfg $HOME/.config/i3status/config
