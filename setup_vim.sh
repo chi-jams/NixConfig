e602f941b9d674d9cb6aef23b1cabf3646903b05
Daichi Jameson <djameson@mines.edu>
2019-02-10 22:28:00 -0700

    removed stuff from setup scripts

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,8 +1,3 @@
 
 vimrc_location=$(readlink -f vimrc)
 ln -s $vimrc_location $HOME/.vimrc
-
-monokai_location=$(readlink -f monokai.vim)
-mkdir $HOME/.vim
-mkdir $HOME/.vim/colors
-ln -s $monokai_location $HOME/.vim/colors/monokai.vim
--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,5 +1,3 @@
 
-chsh $USER -s /bin/zsh
-
 zshrc_location=$(readlink -f zshrc)
 ln -s $zshrc_location $HOME/.zshrc
