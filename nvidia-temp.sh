9fcb7f7ad32ee78c4aaf0d6ac37535f64eb50826
Daichi Jameson <djameson@mines.edu>
2019-02-11 22:35:52 -0700

    Temporary Nvidia settings

---
+++ nvidia-temp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nvidia-settings --assign CurrentMetaMode="DP-4: nvidia-auto-select +1920+0 { ForceFullCompositionPipeline = On },DP-1: nvidia-auto-select +4480+0 { ForceFullCompositionPipeline = On },DP-3: nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
