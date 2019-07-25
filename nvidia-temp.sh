ce2c201158295e7c606dceb18b8db44d113a3182
Daichi Jameson <djameson@mines.edu>
2019-07-24 19:54:04 -0600

    Changed shitwidth for typescript files in vim, changed nvidia script for new monitor layout, and changed i3config to use firefox and not autolock when audio is playing

--- nvidia-temp.sh
+++ nvidia-temp.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-nvidia-settings --assign CurrentMetaMode="DP-4: nvidia-auto-select +1920+0 { ForceFullCompositionPipeline = On },DP-1: nvidia-auto-select +4480+0 { ForceFullCompositionPipeline = On },DP-3: nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
+nvidia-settings --assign CurrentMetaMode="DP-2: nvidia-auto-select +1920+0 {
+ForceFullCompositionPipeline = On },HDMI-0: nvidia-auto-select +4480+0 { ForceFullCompositionPipeline = On },DP-5: nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
