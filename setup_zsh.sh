1703df99e1921c9c39b32b83c7c7c5b6fad91856
Chi Jameson <chi@tinychaitea.com>
2024-03-07 15:16:50 -0700

    Added stuff for install ohmyzsh

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,3 +1,6 @@
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 zshrc_location=$(readlink -f zshrc)
 ln -s $zshrc_location $HOME/.zshrc
