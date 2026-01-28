9c957411c9ba0c31547a2b97cdd0a5399a873a12
Chi Jameson <chi@tinychaitea.com>
2026-01-27 17:45:58 -0700

    Removed unused files

--- setup_git.sh
+++
@@ -1,4 +0,0 @@
-
-gitconfig_location=$(readlink -f gitconfig)
-ln -sf $gitconfig_location $HOME/.gitconfig
-
--- setup_zsh.sh
+++ setup_zsh.sh
@@ -3,4 +3,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 zshrc_location=$(readlink -f zshrc)
-ln -s $zshrc_location $HOME/.zshrc
+ln -fs $zshrc_location $HOME/.zshrc
