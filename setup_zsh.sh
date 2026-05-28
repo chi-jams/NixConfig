bdcc2e874ba91bfb7be76013ac4136cb4ccbb7e2
Chi Jameson <bct@tinychaitea.com>
2026-05-28 17:44:16 -0600

    Added reminders of what needed to be set up for later

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,4 +1,15 @@
 
+# -- Alacritty check --
+echo "NOTE: These configurations are designed to run on Alacritty."
+if ! command -v alacritty &> /dev/null; then
+  echo "WARNING: 'alacritty' is not installed. Install it with your package manager, e.g.:"
+  echo "  sudo pacman -S alacritty"
+fi
+
+# -- Nerd Fonts reminder --
+echo "REMINDER: Powerlevel10k requires Nerd Fonts. Install the recommended font:"
+echo "  https://github.com/romkatv/powerlevel10k#fonts"
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
